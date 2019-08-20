2e682db104c21a33abd6a05c0b0cbb8701c40ccd
Michael Ledin <mledin89@gmail.com>
2019-08-20 03:12:54 +0300

    Remove directories before project or page loading.

--- src/TildaLoader.php
+++ src/TildaLoader.php
@@ -12,10 +12,27 @@ function resolvePath(...$pathParts)
     return join(DIRECTORY_SEPARATOR, $pathParts);
 }
 
+function rrmdir($dir)
+{
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir . DIRECTORY_SEPARATOR . $object))
+                    rrmdir($dir . DIRECTORY_SEPARATOR . $object);
+                else
+                    unlink($dir . DIRECTORY_SEPARATOR . $object);
+            }
+        }
+        rmdir($dir);
+    }
+}
+
 class TildaLoader
 {
     const CONFIG_OPTION_PATH = 'path';
-    const CONFIG_HTML_FILE_NAME = 'index.html';
+    const CONFIG_OPTION_HTML_FILE_NAME = 'htmlFileName';
+    const DEFAULT_HTML_FILE_NAME = 'index.html';
 
     /** @var TildaApi */
     protected $client;
@@ -33,7 +50,7 @@ class TildaLoader
     {
         $this->client = $client;
         $this->path = $config[self::CONFIG_OPTION_PATH];
-        $this->htmlFileName = $config[self::CONFIG_HTML_FILE_NAME] ?? 'index.html';
+        $this->htmlFileName = $config[self::CONFIG_OPTION_HTML_FILE_NAME] ?? self::DEFAULT_HTML_FILE_NAME;
 
         if (!$this->path) {
             throw TildaLoaderInvalidConfigurationException::forConfigOption(self::CONFIG_OPTION_PATH);
@@ -42,6 +59,7 @@ class TildaLoader
 
     /**
      * @param int $projectId
+     * @param bool $clean
      * @throws AssetLoadingException
      * @throws AssetStoringException
      * @throws Exceptions\Api\TildaApiException
@@ -49,17 +67,22 @@ class TildaLoader
      * @throws Exceptions\Map\MapperNotFoundException
      * @throws PageNotLoadedException
      */
-    public function project(int $projectId)
+    public function project(int $projectId, bool $clean = true)
     {
         $pages = $this->client->getPagesList($projectId);
 
+        if ($clean) {
+            rrmdir($this->getProjectPath($projectId));
+        }
+
         foreach ($pages as $page) {
-            $this->page($page->id);
+            $this->page($page->id, false);
         }
     }
 
     /**
      * @param int $pageId
+     * @param bool $clean
      * @return Objects\Page\ExportedPage|null
      * @throws AssetLoadingException
      * @throws AssetStoringException
@@ -68,7 +91,7 @@ class TildaLoader
      * @throws Exceptions\Map\MapperNotFoundException
      * @throws PageNotLoadedException
      */
-    public function page(int $pageId)
+    public function page(int $pageId, bool $clean = true)
     {
         $pageInfo = $this->client->getPageFullExport($pageId);
         if (!$pageInfo) {
@@ -76,6 +99,10 @@ class TildaLoader
         }
 
         $pagePath = $this->getPagePath($pageInfo->projectid, $pageId);
+
+        if ($clean) {
+            rrmdir($pagePath);
+        }
 
         $this->loadFiles($pageInfo->css ?? [], $pagePath);
         $this->loadFiles($pageInfo->js ?? [], $pagePath);
